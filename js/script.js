cd01feead78f8d969399e994606c2ba900cd9d0b
ParkerWSmith <smithparker0@gmail.com>
2017-04-19 17:16:46 -0500

    Finished

--- js/script.js
+++ js/script.js
@@ -1,24 +1,19 @@
 $(document).ready(function () {
 
-    var addStuffHere = "#addstuff";
-    var subjectHeaders = 'h2';
-    var createMyFavThings = function (subject, description, videoID, figText, myClass) {
+    const addStuffHere = "#addstuff";
+    const subjectHeaders = 'h2';
+    const createMyFavThings = function (subject, description, videoID, figText, myClass) {
 
-        var favVideoID = "#" + videoID;
+        const favVideoID = "#" + videoID;
 
         $(addStuffHere).append('<div class="row">');
         $(addStuffHere).append('<div id= "' + videoID + '" class="col-xs-6 col-md-4">');
-
         $(favVideoID).append('<h2 id="' + subject + '">' + subject + '</h2>')
-
         $(subjectHeaders).hide();
-
         $(favVideoID).append('<p class="' + myClass + '">' + description + '</p>');
         $('p').hide();
-
         $(addStuffHere).append('<div id="video ' + videoID + ' " class="col-xs-6 col-md-4">');
         $(favVideoID).append('<iframe class="' + myClass + '" src="https://www.youtube.com/embed/' + videoID + '"></iframe>');
-
         $('iframe').hide();
         $(addStuffHere).append('<figcaption class="' + myClass + '">' + figText + '</figcaption');
         $('figcaption').hide();
@@ -64,7 +59,7 @@ $(document).ready(function () {
     $('#addstuff').append('<button id="favoritethingbutton">Click here to see my favorite things</button>');
 
     $('button').click(function () {
-        $(this).remove();
+                $(this).remove();
         $(subjectHeaders).show();
     });
 
